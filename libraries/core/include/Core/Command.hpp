7929fe71991d225d061282d10d283d3ef8a09611
Jason Cresp <698878-jcresp@users.noreply.gitlab.com>
2023-12-29 13:36:38 +1100

    Update Command class to return reference to const for member variables

--- libraries/core/include/Core/Command.hpp
+++ libraries/core/include/Core/Command.hpp
@@ -9,8 +9,8 @@ namespace Phalanx::Core {
     public:
         Command() = delete;
         explicit Command(TokenisedIdentifier identifier, std::string body = std::string{ "" });
-        auto Body() const -> std::string;
-        auto Identifier() const -> TokenisedIdentifier;
+        auto Body() const -> const std::string&;
+        auto Identifier() const -> const TokenisedIdentifier&;
     protected:
     private:
         std::string body_;
--- libraries/core/source/Core/Command.cpp
+++ libraries/core/source/Core/Command.cpp
@@ -4,11 +4,11 @@ namespace Phalanx::Core {
     Command::Command(TokenisedIdentifier identifier, std::string body) 
         : identifier_{ identifier }, body_{ body } {}
 
-    auto Command::Body() const -> std::string {
+    auto Command::Body() const -> const std::string& {
         return body_;
     }
 
-    auto Command::Identifier() const -> TokenisedIdentifier {
+    auto Command::Identifier() const -> const TokenisedIdentifier& {
         return identifier_;
     }
 }	// namespace Phalanx::Core
