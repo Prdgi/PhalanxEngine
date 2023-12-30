26739069acd04b268304ee0d7ec90ed421147d86
Jason Cresp <698878-jcresp@users.noreply.gitlab.com>
2023-12-30 13:44:06 +1100

    Phalanx::Core::Logbook Rule of 0/3/5

--- libraries/core/test/unit_tests/Core/Logbook.test.cpp
+++ libraries/core/test/unit_tests/Core/Logbook.test.cpp
@@ -2,4 +2,23 @@
 #include "Core/Logbook.hpp"					/// Class to test
 
 namespace Phalanx::Core {
+    TEST_CASE("Phalanx::Core::Logbook Rule of 0/3/5") {
+        SECTION("Default constructor should be enabled") {
+            REQUIRE(std::is_default_constructible<Logbook>::value == true);
+        }
+
+        SECTION("Class should be destructible") {
+            REQUIRE(std::is_destructible<Logbook>::value == true);
+        }
+
+        SECTION("Copy semantics should be enabled") {
+            REQUIRE(std::is_copy_constructible<Logbook>::value == true);
+            REQUIRE(std::is_copy_assignable<Logbook>::value == true);
+        }
+
+        SECTION("Move semantics should be enabled") {
+            REQUIRE(std::is_move_constructible<Logbook>::value == true);
+            REQUIRE(std::is_move_assignable<Logbook>::value == true);
+        }
+    }
 }	// namespace Phalanx::Core
